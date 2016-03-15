2f0d01d7359388e56f99e7e52256e7e2f46549ca
BuildingAtom <84liadam@gmail.com>
2016-03-15 10:45:42 -0400

    More stuff for login. Complete the javscript includes.

--- account/login/index.php
+++ account/login/index.php
@@ -1,1 +1,49 @@
-<link rel="canonical" href="https://alexcican.com/post/single-post">
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="canonical" href="/account/login/">
+	<title>Freedom Robotics Login</title>
+<?php
+	include($_SERVER['DOCUMENT_ROOT']."/includes/jsincludes.php");
+	includeJS("core:1.12.0", "", "", "", "", "", "1.6.16");
+	?>
+	<link href="./content/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<div id="nav" class="disableHighlight"> <!--The top navigation coolio stuff-->
+		<div id="nav_home" style="height: 30px; width: 30px;">
+			<img src="./img/homebutton.svg" style="z-index: 100; height: 30px; width: 30px;" alt="">
+			<img id="homeSVG" src="./img/homebuttonhover.svg" style="z-index: 101; opacity: 0; height: 30px; width: 30px;" alt="">
+		</div>
+		<div id="nav_back" style="height: 60px; width: 60px; top: 30px; left: 30px;">
+			<img src="./img/backarrow.svg" style="z-index: 100; height: 60px; width: 60px;" alt="">
+			<img id="backSVG" src="./img/backarrowhover.svg" style="z-index: 101; height: 60px; width: 60px; opacity: 0;" alt="">
+		</div>
+	</div>
+	<script type="text/javascript">
+	$("#nav_home").hover(function() {
+		$("#nav_home").animate({"height": "60px", "width": "60px"}, 200).children().animate({"height": "60px", "width": "60px"}, 200);
+		$("#nav_back").animate({"height": "30px", "width": "30px", "top": "60px", "left": "60px"}, 200).children().animate({"height": "30px", "width": "30px"}, 200);
+		$("#homeSVG").css("opacity", 1);
+	},function() {
+		$("#nav_home").animate({"height": "30px", "width": "30px"}, 200).children().animate({"height": "30px", "width": "30px"}, 200);
+		$("#nav_back").animate({"height": "60px", "width": "60px", "top": "30px", "left": "30px"}, 200).children().animate({"height": "60px", "width": "60px"}, 200);
+		$("#homeSVG").css("opacity", 0);
+	});
+	$("#nav_back").hover(function() {
+		$("#backSVG").css("opacity", 1);
+	},function() {
+		$("#backSVG").css("opacity", 0);
+	});
+	$("#nav_home").click(function() {
+		window.location = "http://trhcreations.tk/";
+	});
+	$("#nav_back").click(function() {
+		history.back(-1);
+	});
+	</script>
+	<div id="content">
+		
+	</div>
+</body>
--- includes/jsincludes.php
+++ includes/jsincludes.php
@@ -1,6 +1,60 @@
 <?php
-
 // Includes javascript libraries and frameworks as specified.
-if ($_GET["JQUERY"] == true){
-	if ($_GET["JQ_EXT"] == "")
+function includeJS($JQUERY, $ANGULAR, $DOJO, $MOO, $SPF, $THREE, $WEBFONT){
+	//JQuery Libraries
+	$jQueryLibs = explode(",", preg_replace('/\s+/', '', $JQUERY));
+	foreach ($jQueryLibs as $var){
+		$lib = explode(":", $var, 2);
+		if(strcasecmp($lib[0],"core") == 0){
+			echo "	<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/", $lib[1],"/jquery.min.js\"></script>\n";
+		}
+		if(strcasecmp($lib[0],"ui") == 0){
+			echo "	<link rel=\"stylesheet\" href=\"https://ajax.googleapis.com/ajax/libs/jqueryui/", $lib[1],"/themes/smoothness/jquery-ui.css\">\n";
+			echo "	<script src=\"https://ajax.googleapis.com/ajax/libs/jqueryui/", $lib[1],"/jquery-ui.min.js\"></script>\n";
+		}
+		if(strcasecmp($lib[0],"mobile") == 0){
+			echo "	<link rel=\"stylesheet\" href=\"https://ajax.googleapis.com/ajax/libs/jquerymobile/", $lib[1],"/jquery.mobile.min.css\">\n";
+			echo "	<script src=\"https://ajax.googleapis.com/ajax/libs/jquerymobile/", $lib[1],"/jquery.mobile.min.js\"></script>\n";
+		}
+	}
+	unset ($var);
+
+	//AngularJS Libraries
+	$AngularLibs = explode(",", preg_replace('/\s+/', '', $ANGULAR));
+	foreach ($AngularLibs as $var){
+		$lib = explode(":", $var, 2);
+		if(strcasecmp($lib[0],"core") == 0){
+			echo "	<script src=\"https://ajax.googleapis.com/ajax/libs/angularjs/", $lib[1],"/angular.min.js\"></script>\n";
+		}
+		if(strcasecmp($lib[0],"material") == 0){
+			echo "	<script src=\"https://ajax.googleapis.com/ajax/libs/angular_material/", $lib[1],"/angular-material.min.js\"></script>\n";
+		}
+	}
+	unset ($var);
+
+	//Dojo Toolkit
+	if ($DOJO != ""){
+		echo "	<script src=\"https://ajax.googleapis.com/ajax/libs/dojo/", $DOJO, "/dojo/dojo.js\"></script>\n";
+	}
+
+	//MooTools
+	if ($MOO != ""){
+		echo "	<script src=\"https://ajax.googleapis.com/ajax/libs/mootools/", $MOO, "/mootools.min.js\"></script>\n";
+	}
+
+	//SPF
+	if ($SPF != ""){
+		echo "	<script src=\"https://ajax.googleapis.com/ajax/libs/spf/", $SPF, "/spf.js\"></script>\n";
+	}
+
+	//Three.js
+	if ($THREE != ""){
+		echo "	<script src=\"https://ajax.googleapis.com/ajax/libs/threejs/", $THREE, "/three.min.js\"></script>\n";
+	}
+
+	//Web font loader
+	if ($WEBFONT != ""){
+		echo "	<script src=\"https://ajax.googleapis.com/ajax/libs/webfont/", $WEBFONT, "/webfont.js\"></script>\n";
+	}
 }
+?>
